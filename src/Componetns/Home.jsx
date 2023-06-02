2d140771ce279f0eb94e3a8be6e5bb1c0e06cc68
mruthyunjayacolab <mruthyunjayacolab@gmail.com>
2023-06-02 17:27:05 +0530

    css file upadted 2

--- src/Componetns/Home.jsx
+++ src/Componetns/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
         <div className="home"  >
             < Category />
             <img src={img} style={{ width: "100%", height: "200px" }} alt="" />
+            <img src="../images/bannerimafes.jpg" alt="" />
         </div >
     );
 }
